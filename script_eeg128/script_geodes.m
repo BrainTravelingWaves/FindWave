07e28203c4c5a88484f231c87dec20037f57f3ae
Vitaly Verkhlyutov <braintravelingwaves@gmail.com>
2018-10-06 18:09:32 +0300

    Add files via upload

--- script_eeg128/script_geodes.m
+++ script_eeg128/script_geodes.m
@@ -5,18 +5,20 @@ NNN=2000;
 NN=round(N/NNN);
 N=rem(N,NNN);
 dis=zeros(length(Acl),NNN);
+k=0;
 for j=1:NN
   parfor i=1:NNN 
-     dis(:,i)=graphshortestpath(Acl,i,'Directed', false); 
+     dis(:,i)=graphshortestpath(Acl,k+i,'Directed', false); 
   end
-  save(strcat('dis',num2str(j)),'dis');
-end;
+  k=k+NNN;
+  save(strcat('disL',num2str(j)),'dis');
+end
 j=j+1;
-dis=zeros(length(Acl),N);
+% dis=zeros(length(Acl),N);
 parfor i=1:N 
-   dis(:,i)=graphshortestpath(Acl,i,'Directed', false); 
-end;
-save(strcat('dis',num2str(j)),'dis');
+   dis(:,i)=graphshortestpath(Acl,k+i,'Directed', false); 
+end
+save(strcat('disL',num2str(j)),'dis');
 %% Geodesic distaces Right hem
 A=precalcdist(Br); % Br - CortexRight
 N=length(A);
@@ -24,15 +26,16 @@ NNN=2000;
 NN=round(N/NNN);
 N=rem(N,NNN);
 dis=zeros(length(A),NNN);
+k=0;
 for j=1:NN
    parfor i=1:NNN 
-        dis(:,i)=graphshortestpath(A,i,'Directed', false); 
-   end;
-save(strcat('disR',num2str(j)),'dis');
-end;
-j=j+1;
-dis=zeros(length(A),N);
+        dis(:,i)=graphshortestpath(A,k+i,'Directed', false); 
+   end
+   k=k+NNN;
+   save(strcat('disR',num2str(j)),'dis');
+end
+%dis=zeros(length(A),N);
 parfor i=1:N 
-   dis(:,i)=graphshortestpath(A,i,'Directed', false); 
-end;
+   dis(:,i)=graphshortestpath(A,k+i,'Directed', false); 
+end
 save(strcat('disR',num2str(j)),'dis');
